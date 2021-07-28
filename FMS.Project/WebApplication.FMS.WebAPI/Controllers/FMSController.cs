9fe2b4e9b3e4606881162b4d3d1f32d870e965c6
SuperOzi01 <zshare_187@hotmail.com>
2021-07-29 01:56:31 +0300

    FMS_FirstUpdate
    - This Commit Contains two parts:
    1- Packages Installation:
      - Log4Net
      - JWT
    2- API Setup and test...

---
+++ FMS.Project/WebApplication.FMS.WebAPI/Controllers/FMSController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApplication.FMS.WebAPI.Controllers
+{
+    public class FMSController : ApiController
+    {
+        [Route("api/fms/ping")]
+        [HttpGet]
+        public IHttpActionResult Ping()
+        {
+            return Ok(("Ok" , HttpStatusCode.OK));
+        }
+
+        [Route("api/fms/HelthCheck")]
+        [HttpGet]
+        public IHttpActionResult HelthCheck()
+        {
+            // Here We Should ensure that the connection to DB is running ..  
+            return Ok(("Ok", HttpStatusCode.OK));
+        }
+
+    }
+}
