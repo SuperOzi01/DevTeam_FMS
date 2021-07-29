89f90e28b8b9c8d81cbbfeada9eb037bf386531b
SuperOzi01 <zshare_187@hotmail.com>
2021-07-29 11:55:03 +0300

    ClassLibrary Fixes
    Class Library Type Fixed

---
+++ FMS.Project/WebApplication.FMS.WebAPI/AppFilters/ExceptionFilter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http.Filters;
+
+namespace WebApplication.FMS.WebAPI.AppFilters
+{
+    public class ExceptionFilter : ExceptionFilterAttribute
+    {
+        public override void OnException(HttpActionExecutedContext context)
+        {
+            var msg = "ExceptionFilter { Action: " + context.ActionContext.ActionDescriptor.ActionName + " | Time: " + DateTime.Now.ToShortTimeString() + " }";
+            string exception = context.Exception.InnerException.Message;
+            Console.WriteLine(exception);
+            switch(exception)
+            {
+                case "System.DivideByZeroException":
+                    Console.WriteLine("Divide By Zero Exception");
+                    break;
+            }
+
+        }
+    }
+}
---
+++ FMS.Project/WebApplication.FMS.WebAPI/Controllers/FMSController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+using WebApplication.FMS.WebAPI.AppFilters;
+
+namespace WebApplication.FMS.WebAPI.Controllers
+{
+    public class FMSController : ApiController
+    {
+        [Route("api/fms/ping")]
+        [HttpGet]
+        [ExceptionFilter]
+        public IHttpActionResult Ping()
+        {
+
+            throw new DivideByZeroException();
+            return Ok(("Ok" , HttpStatusCode.OK));
+        }
+
+        [Route("api/fms/HelthCheck")]
+        [HttpGet]
+        public IHttpActionResult HelthCheck()
+        {
+            // Here We Should ensure that the connection to DB is running ..  
+            return Ok(("Ok", HttpStatusCode.OK));
+        }
+
+    }
+}
