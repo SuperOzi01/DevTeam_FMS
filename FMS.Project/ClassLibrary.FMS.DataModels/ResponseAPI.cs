4e2850b1cd28af2ec547ec5809b85026f1537ee2
rab110 <rana-alhamdan@hotmail.com>
2021-07-29 16:25:19 +0300

    Authorization filter
    done the Authorization filter

---
+++ FMS.Project/ClassLibrary.FMS.DataModels/ResponseAPI.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary.FMS.DataModels
+{
+    public class ResponseAPI
+    {
+        public bool Result { get; set; }
+        public string Message { get; set; }
+    }
+}
---
+++ FMS.Project/WebApplication.FMS.WebAPI/App_Start/TokenManager.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Web;
+
+namespace WebApplication.FMS.WebAPI.App_Start
+{
+    public class TokenManager
+    {
+        private static string Secret = "ERMN05OPLoDvbTTa/QkqLNMI7cPLguaRyHzyg7n5qNBVjQmtBhz4SzYh4NBVCXi3KJHlSXKP+oi2+bXr6CUYTR==";
+        public static string GenerateToken()
+        {
+           // Book_StoreEntities Con = new Book_StoreEntities();
+            //var nameRole = Con.RoleName(id).ToList();
+            byte[] key = Encoding.ASCII.GetBytes(Secret);
+            SymmetricSecurityKey securityKey = new SymmetricSecurityKey(key);
+
+            List<Claim> claims = new List<Claim>();
+            //foreach (var item in nameRole)
+            //{
+             //   claims.Add(new Claim(ClaimTypes.Role, item));
+            //}
+
+            SecurityTokenDescriptor descriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.UtcNow.AddMinutes(30),
+                SigningCredentials = new SigningCredentials(securityKey,
+                SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            JwtSecurityTokenHandler handler = new JwtSecurityTokenHandler();
+            JwtSecurityToken token = handler.CreateJwtSecurityToken(descriptor);
+            return handler.WriteToken(token);
+        }
+    }
+}
