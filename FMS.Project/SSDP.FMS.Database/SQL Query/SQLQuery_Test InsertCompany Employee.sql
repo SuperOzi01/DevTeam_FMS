286e711f00a407724cd1fc416b8eebc17c3f2477
SuperOzi01 <zshare_187@hotmail.com>
2021-08-01 14:49:20 +0300

    Stored Procedures Progress
    - Insert Company Employee
    - Insert Beneficiary

---
+++ FMS.Project/SSDP.FMS.Database/SQL Query/SQLQuery_Test InsertCompany Employee.sql
@@ -0,0 +1,4 @@
+﻿Exec dbo.SP_InsertCompanyEmployee @username = "TestStored", @Password = "1234", @SpecializationID = 1, @RoleID = 2, @LocationID = 1, @ManagerID = null 
+
+select * from dbo.CompanyEmployee
+
---
+++ FMS.Project/SSDP.FMS.Database/StoredProcedures/SP_InsertBeneficiary.sql
@@ -0,0 +1,10 @@
+﻿CREATE PROCEDURE [dbo].[SP_InsertBeneficiary]
+	@Username nchar (40),
+	@Password nchar (40), 
+	@BuildingID INT,
+	@RoleID INT
+AS
+	BEGIN
+		INSERT INTO dbo.Beneficiary(Username, Password, Building_BuildingID, Role_RoleID)
+		VALUES (@Username, @Password, @BuildingID, @RoleID)
+	END
