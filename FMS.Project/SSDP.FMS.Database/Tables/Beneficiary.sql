33a8c6d84987a93a9118d5f8439e40b6c4ebfcc4
SuperOzi01 <zshare_187@hotmail.com>
2021-08-01 14:22:00 +0300

    Database Creation
    - This Patch contains the creation of the database tables and there relations.

---
+++ FMS.Project/SSDP.FMS.Database/Tables/Beneficiary.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Beneficiary]
+(
+	[BeneficiaryID] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Username] NCHAR(40) NOT NULL, 
+    [Password] NCHAR(40) NOT NULL, 
+    [Building_BuildingID] INT NOT NULL, 
+    [Role_RoleID] INT NOT NULL, 
+    CONSTRAINT [FK_Beneficiary_Building] FOREIGN KEY ([Building_BuildingID]) REFERENCES [Building]([BuildingID]), 
+    CONSTRAINT [FK_Beneficiary_Role] FOREIGN KEY ([Role_RoleID]) REFERENCES [Role]([RoleID])
+)
