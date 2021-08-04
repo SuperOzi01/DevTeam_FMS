5b3d98be73474b326a27849c43ac319a6b688009
rab110 <rana-alhamdan@hotmail.com>
2021-08-04 16:57:28 +0300

    .

--- FMS.Project/WebApplication.FMS.MVC/Controllers/LoginController.cs
+++ FMS.Project/WebApplication.FMS.MVC/Controllers/LoginController.cs
@@ -3,9 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Net.Http;
-using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using WebApplication.FMS.MVC.Filters;
 
